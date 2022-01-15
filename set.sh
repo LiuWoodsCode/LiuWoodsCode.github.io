65bed2ee611c13f3eeb7ee2d876bcefb9feb8ae9
LiuWoodsCode <68132166+LiuWoodsCode@users.noreply.github.com>
2022-01-15 13:27:35 -0500

    Create set.sh

---
+++ set.sh
@@ -0,0 +1,10 @@
+qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript '
+    var allDesktops = desktops();
+    for (i=0;i<allDesktops.length;i++) 
+    {
+        d = allDesktops[i];
+        d.wallpaperPlugin = "org.kde.image";
+        d.currentConfigGroup = Array("Wallpaper", "org.kde.image", "General");
+        d.writeConfig("Image", "file:///home/mycroft/Downloads/IMG_0172_1.jpg")
+    }
+'
