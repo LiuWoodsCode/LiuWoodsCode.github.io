74841799ff5861050bc2e10d9c70676e845654df
Liu Woods <68132166+LiuWoodsCode@users.noreply.github.com>
2023-02-26 12:53:44 -0500

    Create blog.js

---
+++ blog.js
@@ -0,0 +1,12 @@
+console.log("I forgave my brother for trying to kill me. I understand the urges he gets, I’ve fallen for the same ones time and time again in the past, so who am I to really judge him, anyway? -Homicidal Liu");
+
+document.addEventListener('keydown', function(event) {
+  if (event.ctrlKey && event.key === '\\') {
+    const newTab = window.open('https://liuwoodscode.github.io/');
+    newTab.document.title = 'Homicidal Liu ;)';
+    newTab.addEventListener('load', function() {
+      alert('Homicidal Liu is here for you and is ready with a knife!');
+    });
+  }
+});
+
